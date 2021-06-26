510a771d54cc3168228d079ab42761608a5f735c
Dev Bilaspure <devbilaspure4941@gmail.com>
2021-06-26 12:50:36 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a=2;
+    int b=3;
+
+    cout<<"The product is: "<<a*b;
+    return(0);
+}
