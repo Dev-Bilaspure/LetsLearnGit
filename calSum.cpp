5e1ad223300ae876db5600b537079c54f73ed793
Dev Bilaspure <devbilaspure4941@gmail.com>
2021-06-26 11:47:29 +0530

    initial commit

---
+++ calSum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a,b;
+    cout<<"Enter two number: ";
+    cin>>a>>b;
+
+    cout<<"Sum is: "<<a+b;
+    return(0);
+}
